2c8d06e90c64ef673f45414c9736e11ceb197896
Lucchetto <lucchetto.tie@live.com>
2020-09-11 16:24:12 +0000

    miatoll: revengeos-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_miatoll.mk
+    $(LOCAL_DIR)/revengeos_miatoll.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_miatoll-user \
-    lineage_miatoll-userdebug \
-    lineage_miatoll-eng
+    revengeos_miatoll-eng
