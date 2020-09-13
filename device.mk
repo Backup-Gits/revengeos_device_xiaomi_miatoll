5d216d5e0b1b82a30ea5fda35b2fddd946bec7b5
4PERTURE <slicey2001@gmail.com>
2020-09-13 07:40:14 +0530

    build libhwbinder

--- device.mk
+++ device.mk
@@ -311,6 +311,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
+# libhwbinder
+PRODUCT_PACKAGES += \
+    libhwbinder.vendor \
+    libhwbinder
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_atoll
